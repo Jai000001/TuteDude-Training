027626eab8f88823b9d2f164d51665155b5d9fe1
Jai000001 <153719659+Jai000001@users.noreply.github.com>
2025-06-06 08:27:54 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,6 +1,6 @@
-a = input("Enter the first number: ")#takes first number as input from user
+a = input("Enter the first number: ") #takes first number as input from user#
 a = int(a)#stores it as integer value
-b = input("Enter the second number: ")#takes second number as input from user
+b = input("Enter the second number: ") #takes second number as input from user#
 b = int(b)#stores it as integer value
 print("Addition: ", a+b)
 print("Subtraction: ", a-b)
